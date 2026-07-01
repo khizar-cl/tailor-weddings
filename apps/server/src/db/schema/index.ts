61d35d9b5b70f7c3c60ab4c033666b1a25b7f702
Khizar Nawab <khizarnawab@Khizars-MacBook-Pro.local>
2026-07-01 16:44:47 +0500

    feat(server): add vendor drizzle schemas

--- apps/server/src/db/schema/index.ts
+++ apps/server/src/db/schema/index.ts
@@ -1,2 +1,5 @@
 export * from "./files.schema";
+export * from "./service-packages.schema";
 export * from "./users.schema";
+export * from "./vendor-images.schema";
+export * from "./vendors.schema";
